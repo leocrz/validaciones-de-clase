98b47574b2bc1d90a34bc601ae0ea17cdaeb8bc5
JOSE LEONEL CRUZ LOPEZ <jcruzl20@miumg.edu.gt>
2025-03-28 00:16:23 -0600

    Add files via upload

---
+++ validaciones de clase/validaciones de clase/p1bpoo/p1bpoo/MisClases/Camion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1bpoo.MisClases
+{
+    internal class Camion : Vehiculo
+    {
+            private int horasUsoMotor;
+            private int cargaActual;
+            private int desgastesdefrenos;
+            
+
+
+        public Camion(int anio, string elColor, string elModelo) : base(anio, elColor, elModelo)
+            {
+            tiposLicenciaAceptados = new List<string> { "A" , "a" };
+            horasUsoMotor = 0;
+            cargaActual = 0;
+            desgastesdefrenos = 0;
+            velocidadMaxima = 100; // km/h
+            CapacidadTanque = 200; // km por galó
+            ConsumoCombustible = 10; // En kilómetros por galón
+
+
+        }
+        public void registrarHorasMotor(int horas)
+            {
+                if (horas > 0)
+                {
+                    horasUsoMotor += horas;
+                    Console.WriteLine("La cantidad de horas de uso del motor es {0}", horas);
+                }
+                else
+                {
+                    Console.WriteLine("No se puede registrar una cantidad negativa de horas ");
+                }
+            }
+
+            public void cargar(int cuanto)
+            {
+                if (cuanto > 0)
+                {
+
+                    cargaActual += cuanto;
+                    Console.WriteLine("la cantidad de la carga es {0}", cargaActual);
+                }
+                else
+                {
+                    Console.WriteLine("No se puede cargar una cantidad negativa ");
+                }
+            }
+
+        
+    }
+}
---
+++ validaciones de clase/validaciones de clase/p1bpoo/p1bpoo/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using p1bpoo.MisClases;
+
+//Vehiculo miCarrito = new Vehiculo(2026,"Azul","Alfa Romeo");
+
+//Vehiculo elOtro = new Vehiculo(2000, "Rojo", "DELOREAN");
+
+//CarroElectrico miBYD = new CarroElectrico(2026, "Amarillo", "BYD");
+
+//miBYD.InformacionVehiculo();
+//miBYD.cargarBateria();
+//Console.WriteLine("El nivel de bateria es: {0}",miBYD.NivelBateria());
+Chofer piloto1 = new Chofer("patricia", 28, "A");
+Chofer piloto2 = new Chofer("Karen", 24, "M");
+
+piloto1.mostrarInformacion();
+piloto2.mostrarInformacion();
+
+Vehiculo sedan = new Vehiculo(2025, "Rojo", "Honda");
+
+string respuesta;
+respuesta = sedan.AsignarPiloto(piloto1);
+Console.WriteLine(respuesta);
+respuesta = sedan.Encender();
+Console.WriteLine(respuesta);
+
+Console.WriteLine("\n");
+Chofer chofer = new Chofer("Portillo", 25, "B");
+chofer.mostrarInformacion();
+chofer.CambiarLicencia("A");
+chofer.mostrarInformacion();
+
+
+
+
+sedan.acelerar(25);
+Moto honda = new Moto(2025, "negro", "Kawasaki");
+
+
